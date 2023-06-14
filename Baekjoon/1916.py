c1d51f1030f6e38bfdab5a2af72db2f874d777e7
Leejaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2023-06-14 16:25:13 +0900

    Create 1916.py

---
+++ Baekjoon/1916.py
@@ -0,0 +1,12 @@
+n = int(input())
+m = int(input())
+class Edge:
+    def __init__(self, to, cost):
+        self.to = to
+        self.cost = cost
+costs = [[] for _ in range(n+1)]
+for i in range(m):
+    info = input().split(' ')
+    costs[int(info[0])].append(Edge(int(info[1]), int(info[2])))
+
+s,d = input().split(' ')
