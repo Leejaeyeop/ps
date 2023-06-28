a4a46d0c5ac95f94786340f5a936d673dd80e81d
Leejaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2023-06-28 12:38:01 +0900

    Update 4386.py

--- Baekjoon/4386.py
+++ Baekjoon/4386.py
@@ -1,1 +1,11 @@
 # https://acmicpc.net/problem/4386
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+list = [[] for _ in range(n+1)]
+for i in range(1, n+1):
+    # x 좌표, y 좌표
+    list[i].append((map(int, input().split())))
+     
+print(list)
