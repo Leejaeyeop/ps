fb98a854ecf4fd42c8c385e8c6a6ac90bba970ed
Leejaeyeop <dlwoduq78@naver.com>
2023-07-13 20:43:39 +0900

    2325 input 작성

--- Baekjoon/2352.py
+++ Baekjoon/2352.py
@@ -1,1 +1,10 @@
 # https://www.acmicpc.net/problem/2352
+
+# 1번 연결 yes or no,,,
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+connected = [0]*(n)
+
+targets = list(map(int, input().split()))
