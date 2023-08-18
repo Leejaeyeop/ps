d2f6237ac8c8eead3ab20eb3f681af7074665318
Leejaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2023-08-18 14:18:12 +0900

    13904 풀이

--- Baekjoon/13904.py
+++ Baekjoon/13904.py
@@ -1,1 +1,26 @@
 #  https://www.acmicpc.net/problem/13904
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+answer = 0
+n = int(input())
+finalDay = 0
+list = [[] for _ in range(1001)]
+
+for _ in range(n):
+    d,w = map(int,input().split())
+    list[d].append(w)
+    finalDay = max(finalDay, d)
+
+pq = []
+for day in range(finalDay, 0, -1):
+    for score in list[day]:
+        heapq.heappush(pq, -score)
+
+    if len(pq)>0:
+        score = -heapq.heappop(pq)
+        answer += score
+
+print(answer)    
