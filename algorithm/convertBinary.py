adf3029ee773c07d0587e2ca85e38ee4c4c00831
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2023-10-16 14:39:31 +0900

    Update convertBinary.py

--- algorithm/convertBinary.py
+++ algorithm/convertBinary.py
@@ -1,6 +1,10 @@
 def convertBinary(n, k):
     res = ""
-    while k <= n:
+    while 0 < n:
+        # 나머지
         remainder = n % k
+        # 나머지 + res
         res = str(remainder) + res 
+        # n은 몫으로 
         n = n // k
+    return res  
