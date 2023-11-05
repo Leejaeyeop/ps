2803b1a5f147b0684cc192b274a11f1e2d02918f
Leejaeyeop <dlwoduq78@naver.com>
2023-11-05 15:56:58 +0900

    2473.py 풀이

---
+++ Baekjoon/2473.py
@@ -0,0 +1,38 @@
+import sys
+import bisect
+
+input = sys.stdin.readline
+
+n = int(input())
+
+liquids = list(map(int, input().split()))
+liquids.sort()
+
+value = 3000000004
+answer = [0,0,0]
+
+for i in range(0,len(liquids)-1):
+    left = i + 1
+    right = len(liquids) - 1
+    fixed = liquids[i]
+    # 세 용액 
+    while left < right:
+        a = liquids[left]
+        b = liquids[right]
+
+        sum = a + b + fixed
+
+        if abs(sum) < value:
+            value = abs(sum)
+            answer[0] = fixed
+            answer[1] = a
+            answer[2] = b
+
+        if sum <0:
+            left +=1
+        elif sum>0:
+            right -=1
+        else:
+            break        
+
+print(*answer)
