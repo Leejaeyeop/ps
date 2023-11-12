441e27e77066ad14f01a750d2cde2b6aafab5c61
Leejaeyeop <dlwoduq78@naver.com>
2023-11-12 15:16:12 +0900

    js 백준 input sample 코드 추가

---
+++ sample/beakjoonSample.js
@@ -0,0 +1,15 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function(){ //이 안에 솔루션 코드 작성
+    const A = parseInt(input[0]);
+    const B = parseInt(input[1]);
+    console.log(A+B);
+    process.exit();
+});
