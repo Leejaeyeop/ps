c419c3d8b81f34278090d815827c467d7f0e3ad7
Leejaeyeop <dlwoduq78@naver.com>
2023-11-17 19:51:51 +0900

    beakjoonMultiInput 추가

---
+++ sample/beakjoonMultiInput.js
@@ -0,0 +1,12 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input.push(line)
+}).on('close', function(){
+    
+})
