0873157437c45d9a8de12dc20893521c322b48d8
Leejaeyeop <dlwoduq78@naver.com>
2023-11-28 16:39:40 +0900

    1525.js solved

---
+++ Baekjoon/solved/1525.js
@@ -0,0 +1,75 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+
+readline.on('line', function(line) {
+    input.push(line)
+}).on('close', function(){
+    const d = {}
+    const dx = [0,1,0,-1]
+    const dy = [1,0,-1,0]
+    
+    function locToIdx(x,y) {
+        return (x*3)+y
+    }
+
+    function idxToLoc(idx) {
+        let x = Math.floor(idx / 3)
+        let y = idx % 3
+        return [x,y]
+    }
+
+    function bfs(q,cnt) {
+        let nq = []
+        while(q.length>0) {
+            const str = q.pop()
+
+            if(str === "123456780") {
+                return cnt
+            }
+            const oriIdx = str.indexOf("0")
+            const loc = idxToLoc(str.indexOf("0"))
+
+            for(let i=0; i<4; i++) {
+                let nx = loc[0] + dx[i]
+                let ny = loc[1] + dy[i]
+
+                if(0<= nx && nx<3 && 0<=ny && ny<3) {
+                    const nIdx = locToIdx(nx,ny)
+                    // 방문 확인
+                    let nstr = str.split("")
+
+                    nstr[oriIdx] = nstr[nIdx]
+                    nstr[nIdx] = "0"
+                    nstr = nstr.join("")
+
+                    if(d[nstr] === undefined) {
+                        d[nstr] = 1
+                        nq.push(nstr)
+                    } 
+                }
+            }
+        }
+
+        if(nq.length > 0) {
+            return bfs(nq,cnt+1)
+        } else {
+            return -1
+        }
+
+    }
+
+    let str = ""
+    input.forEach(v => {
+        str += v.split(" ").join("")
+    })
+
+    let q = [str]
+    d[str] = 0
+    console.log(bfs(q,0))
+
+    process.exit()
+})
