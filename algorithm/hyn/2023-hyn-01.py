3534236a36ed230527baba82265c340cbc64f240
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2024-06-15 20:50:13 +0900

    Create 2023-hyn-01.py

---
+++ algorithm/hyn/2023-hyn-01.py
@@ -0,0 +1,66 @@
+import heapq
+import sys
+def solution(k, n, reqs):
+    answer = float("inf")
+    max_cnt = n - k + 1
+    d = [[-1 for _ in range(max_cnt + 1)] for _ in range(k)]
+    req_obj = {}
+
+    for i in range(k):
+        req_obj[i] = []
+    for a, b, c in reqs:
+        req_obj[c - 1].append((a, b))
+    def check_time(mento_distribute, k, reqs):
+        mento = []
+        waiting = 0
+
+        for n in mento_distribute:
+            mento.append([0 for x in range(n)])
+        for s, f, num in reqs:
+            time = heapq.heappop(mento[num-1])
+            waiting += max(0, time-s)
+            heapq.heappush(mento[num-1], max(s, time)+f)
+
+        return waiting
+
+    def get_result(idx, cnt):
+        res = 0
+        on_going = 0
+        q = []
+        if req_obj[idx]:
+            for a, b in req_obj[idx]:
+                if on_going == cnt:
+                    endtime = heapq.heappop(q)
+                    if endtime > a:
+                        res += endtime - a
+                        heapq.heappush(q,endtime+b)
+                    else:
+                        heapq.heappush(q,a+b)
+                        
+                else:
+                    on_going += 1
+                    heapq.heappush(q,a+b)
+
+        d[idx][cnt] = res
+        return res
+
+    def dp(lst):
+        total_sum = 0
+        for i in range(k):
+            if d[i][lst[i]] == -1:
+                get_result(i, lst[i])
+            total_sum += d[i][lst[i]]
+        nonlocal answer
+        answer = min(answer, total_sum)
+
+    def combi(lst, left, idx):
+        for i in range(1, max_cnt + 1):
+            lst[idx] = i
+            if idx < k - 1:
+                combi(lst, left - i, idx + 1)
+            elif idx == k - 1 and left - i == 0:
+                dp(lst)
+
+    combi([0] * k, n, 0)
+
+    return answer
