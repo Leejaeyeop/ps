8e2f6c1f4004fb991efde0cd8e93a6187cc3c82f
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2024-12-08 21:11:04 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 17.00 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,43 @@
+function solution(friends, gifts) {
+    var answer = 0;
+    // 보낸 목록
+    const sentList = {}
+    // 선물지수
+    const giftScore = {}
+
+    for(let friend of friends) {
+        sentList[friend] = {}
+        giftScore[friend] = 0
+        for(let _friend of friends) {
+            if(_friend !== friend) {
+                sentList[friend][_friend] =0
+            }
+        }
+    }
+
+    for(const gift of gifts) {
+        const [a,b] = gift.split(" ")
+        sentList[a][b]++
+
+        giftScore[a]++
+        giftScore[b]--
+    }
+    for(let from in sentList) {
+        let score = 0
+
+        for(let to in sentList[from]) {
+
+            let cur = sentList[from][to]
+            let other = sentList[to][from]
+
+            if(cur > other || (cur === other && giftScore[from] > giftScore[to])) {
+                score++
+            } 
+        }
+        answer = Math.max(answer,score)
+    }
+
+
+
+    return answer;
+}
