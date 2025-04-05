8562abeb309a715c8c882e70e29e392257a9c383
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2025-04-05 15:45:04 +0900

    [level 2] Title: 과제 진행하기, Time: 3.09 ms, Memory: 34.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.js
@@ -0,0 +1,46 @@
+function solution(plans) {
+    const answer = [];
+    
+    const convertTime = (time) => {
+        const [h,m] = time.split(":")
+        return (+(h)*60) + +m
+    }
+    
+    for(let i=0; i<plans.length; i++) {
+        const [name,start,playtime] = plans[i]
+        plans[i][1] = convertTime(start)
+        plans[i][2] = +plans[i][2]
+    }
+    
+    plans.sort((a,b) => b[1] - a[1])
+    const workStack = []
+    let time = 0
+    while(plans.length > 0) {
+ if(workStack.length > 0) {
+            let [name, start, playtime] = workStack.pop()
+            playtime--
+            if(playtime === 0) {
+                answer.push(name)
+            } else {
+                workStack.push([name, start, playtime])
+            }
+        }
+                // 현재 시각에 시작하는 plan이 있는지 확인
+        if(plans.at(-1)[1] === time) {
+            // if(workStack.at(-1) && workStack.at(-1)[2] === 1) {
+            //     const [name] = workStack.pop()
+            //     answer.push(name)
+            // }
+            
+            workStack.push(plans.pop())
+        } 
+        time++
+    }
+    
+    while(workStack.length>0) {
+        const [name] = workStack.pop()
+        answer.push(name)
+    }
+
+    return answer;
+}
