a785e7ad786b0def5db0d6ca654b1db8e64187c1
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2025-04-06 13:53:47 +0900

    [level 1] Title: 과일 장수, Time: 215.44 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+    let answer = 0;
+    
+    score.sort((a,b) => b-a)
+    for(let i=m-1; i<score.length; i+=m) {
+        answer+= (score[i] * m)
+    }
+    
+    return answer;
+}
