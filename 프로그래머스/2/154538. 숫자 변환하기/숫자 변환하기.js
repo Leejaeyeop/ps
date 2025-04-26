35bd0ecaefa2f7fc63a4644f73441e1dba629b1e
jaeyeop <93045720+Leejaeyeop@users.noreply.github.com>
2025-04-26 23:10:09 +0900

    [level 2] Title: 숫자 변환하기, Time: 129.29 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,37 @@
+function solution(x, y, n) {
+    let answer = -1;
+    const visited = Array.from({length:y+1},() => false)
+    
+    const bfs = () => {
+        let stack = [x]
+        const nStack = []
+        let cnt = 0
+        while(stack.length > 0) {
+           while(stack.length > 0) {
+                const num = stack.pop()
+
+                if(num === y) {
+                    return cnt
+                }
+               
+               const nNums = [num*2,num*3,num+n]
+               
+               for(let i=0; i<3; i++) {
+                    if(!visited[nNums[i]] && nNums[i]<=y) {
+                            nStack.push(nNums[i])
+                            visited[nNums[i]] = true
+                     }                   
+               }
+               
+           }
+           stack = [...nStack]
+           nStack.length = 0
+           cnt++
+        }
+        
+        return -1
+        
+    }
+    
+    return bfs();
+}
